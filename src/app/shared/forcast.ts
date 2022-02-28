140b695ff22472d7f547b6f678509dec1609cd75
Gutha Nagalatha <nagalatha.chowdhary@gmail.com>
2022-02-28 15:36:06 +0530

    added interfaces for types

--- src/app/shared/forcast.ts
+++
@@ -1,9 +0,0 @@
-export class Forcast {
-    constructor(
-        public city:Object,
-        public cnt:number,
-        public cod:string,
-        public list:Array<Object>,
-        public message:number
-    ){}
-}
